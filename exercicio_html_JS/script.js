a9c8d566ac4f9932e6aa4eb6da60ed9ea1a76944
AndrewMalcher <andrew.malcher.r@gmail.com>
2023-04-08 15:01:14 -0400

    add

---
+++ exercicio_html_JS/script.js
@@ -0,0 +1,18 @@
+const campoA = document.querySelector("#campoA");
+const campoB = document.querySelector("#campoB");
+const form = document.querySelector("#form");
+
+
+// tira atualização do submit
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+
+    // se o campoB for maior que o campoA, envia o formulário
+    if (campoB.value > campoA.value) {
+        alert("Formulario enviado com sucesso");
+    }
+    else {
+        alert("O Campo B é menor que o Campo A, formulário inválido.")
+    }
+})
