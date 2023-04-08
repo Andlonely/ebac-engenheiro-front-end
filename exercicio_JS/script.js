4a21e9681cfe2244aa3172d23d77f4b0bee7f26c
AndrewMalcher <andrew.malcher.r@gmail.com>
2023-04-08 15:06:29 -0400

    sla

---
+++ exercicio_JS/script.js
@@ -0,0 +1,9 @@
+function calculaArea(base, altura){
+    area = base*altura
+    return area;
+}
+
+let base = prompt("Digite a base: ");
+let altura = prompt("Digite a altura: ");
+
+alert("A Area é: "+calculaArea(base,altura));
