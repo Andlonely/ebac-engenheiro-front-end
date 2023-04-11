bc8e7c7ec709b9d611de3a9847801032db7fb82d
AndrewMalcher <andrew.malcher.r@gmail.com>
2023-04-11 01:12:24 -0400

    fix bug

--- exercicio_html_JS/script.js
+++ exercicio_html_JS/script.js
@@ -2,18 +2,18 @@ const campoA = document.querySelector("#campoA");
 const campoB = document.querySelector("#campoB");
 const form = document.querySelector("#form");
 
-
 // tira atualização do submit
 form.addEventListener("submit", (event) => {
     event.preventDefault()
 
-    const campoA = parseInt(campoA.value);
-    const campoB = parseInt(campoB.value);
-    // se o campoB for maior que o campoA, envia o formulário
-    if (campoB.value >= campoA.value) {
-        alert("Formulario enviado com sucesso");
-    }
-    else {
+    // converte os valores dos campos em números
+    const valorCampoA = parseInt(campoA.value);
+    const valorCampoB = parseInt(campoB.value);
+
+    // se o campoB for maior ou igual ao campoA, envia o formulário
+    if (valorCampoB > valorCampoA) {
+        alert("Formulário enviado com sucesso");
+    } else {
         alert("O Campo B é menor que o Campo A, formulário inválido.")
     }
 })
